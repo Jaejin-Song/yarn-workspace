41c7aaef617389aa28fdd0433b3800fc8a476300
Jaejin Song <songjj77@naver.com>
2024-01-14 20:08:43 +0900

    Feature : 기본적인 구조 설정

---
+++ packages/common-components/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Checkbox from "./components/Checkbox.vue";
+</script>
+<template>
+  <Checkbox />
+</template>
+<style lang="scss" scoped></style>
---
+++ packages/package-1/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { Checkbox } from "@common/components";
+</script>
+<template>
+  <div>This is Package-1.</div>
+  <Checkbox />
+</template>
+<style lang="scss" scoped></style>
